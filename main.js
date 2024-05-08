c222156216ce0c601a8782c01cb4753c42dc7477
EimantasV <eima.eima1244@gmail.com>
2024-05-08 20:40:47 +0300

    unfix

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const main = async () =>
 {
     await EthernetConnection.waitForConnection();
     ServerHTTPS.start();
-    ServerUDP;
     ServerWSS.start({serverHTTPS: ServerHTTPS.server, sendUDP: ServerUDP.send});
 
 };
