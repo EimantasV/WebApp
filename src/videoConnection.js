e2923f0976fbe1ff93bd35ac3f557ff2873aa441
EimantasV <eima.eima1244@gmail.com>
2024-05-09 12:23:16 +0300

    a

--- src/videoConnection.js
+++ src/videoConnection.js
@@ -17,7 +17,7 @@ class VideoConnection {
             console.log("No local video stream, if this is desktop then okay.");
         }
         this.remoteVideo = document.getElementById('remoteVideo');
-        WebSocketConnection.WS.onmessage = VideoConnection.handleServerMessage;
+
     }
 
     static async getVideoStream() {
@@ -44,23 +44,7 @@ class VideoConnection {
             console.log(error);
         }
     }
-        static handleServerMessage(message) {
-        console.log("From server:", message);
-        if (!VideoConnection.peerConnection) VideoConnection.start(false);
 
-        const signal = JSON.parse(message.data);
-
-        if (signal.type === "sdp") {
-            VideoConnection.peerConnection.setRemoteDescription(new RTCSessionDescription(signal.data)).then(() => {
-                // Only create answers in response to offers
-                if (signal.data.type !== 'offer') return;
-
-                VideoConnection.peerConnection.createAnswer().then(VideoConnection.createdDescription);
-            });
-        } else if (signal.type === "ice") {
-            VideoConnection.peerConnection.addIceCandidate(new RTCIceCandidate(signal.data));
-        }
-    }
     static start(isInitializer) {
 
 
--- src/websocketConnection.js
+++ src/websocketConnection.js
@@ -4,6 +4,24 @@ class WebSocketConnection{
     
     static{
         this.WS = new WebSocket(`wss://${window.location.hostname}:3000`);
+        this.WS.onmessage = this.handleServerMessage;
+    }
+    static handleServerMessage(message) {
+        console.log("From server:", message);
+        if (!VideoConnection.peerConnection) VideoConnection.start(false);
+
+        const signal = JSON.parse(message.data);
+
+        if (signal.type === "sdp") {
+            VideoConnection.peerConnection.setRemoteDescription(new RTCSessionDescription(signal.data)).then(() => {
+                // Only create answers in response to offers
+                if (signal.data.type !== 'offer') return;
+
+                VideoConnection.peerConnection.createAnswer().then(VideoConnection.createdDescription);
+            });
+        } else if (signal.type === "ice") {
+            VideoConnection.peerConnection.addIceCandidate(new RTCIceCandidate(signal.data));
+        }
     }
     static close()
     {
